1e38b09684a2d484b14399d31273fa27199858d6
Ma shuo <2690439841@qq.com>
2023-05-31 19:51:40 +0800

    增加了CMakeList.txt的规范性，增加了配置库

---
+++ lib/TimeSys/build/CMakeFiles/TIME_SYSTEM_CPP.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for TIME_SYSTEM_CPP.
+# This may be replaced when dependencies are built.
---
+++ lib/sciplot/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 检查build文件夹是否存在
+if [ -d "build" ]; then
+    # 提示用户是否删除已有的build文件夹
+    read -p "The 'build' directory already exists. Do you want to remove it and create a new one? (y/n): " choice
+
+    # 根据用户的选择执行相应的操作
+    if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+        rm -rf build
+        echo "Removed the existing 'build' directory."
+    else
+        echo "Aborted. Exiting script."
+        exit 0
+    fi
+fi
+
+
+mkdir build
+cd build || exit
+cmake ..
+make -j 4
